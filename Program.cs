b4a81b3faed244289b5cf455608f9742ab8a7dea
Enduren <dtennison79@gmail.com>
2021-11-07 18:16:51 -0600

    commit inital

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace staticclatt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song holiday = new Song("Holiday", "Greenday", 200);
+            Console.WriteLine(Song.songCount);
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+            Console.WriteLine(Song.songCount);
+
+
+
+        }
+    }
+}
---
+++ Song.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace staticclatt
+{
+    class Song
+    {
+
+        public string title;
+        public string artist;
+        public int duration;
+        //create a static attribute for songCount
+        public static int songCount = 0;
+        
+        //constructor method
+        public Song(string aTitle, string aArtist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+           //every time a song object gets created the song count will add 1 to it
+            songCount++;
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
